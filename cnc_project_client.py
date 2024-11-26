246d840d6b01fc5a065e892ed2d6e127d0894849
H4lfLif33 <166639735+H4lfLif33@users.noreply.github.com>
2024-11-25 19:24:54 -0500

    added login
    added login functionality and merged

--- cnc_project_client.py
+++ cnc_project_client.py
@@ -1,10 +1,7 @@
-from enum import Enum
 import os
 import socket
-import rsa
-import pathlib
-import tkinter
 from tkinter import filedialog as fd
+from cryptography.fernet import Fernet
 
 
 IP = "10.180.82.224"
@@ -18,11 +15,12 @@ def main():
     client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     client.connect(ADDR)
     print(client.recv(SIZE).decode(FORMAT))
-
+      
     KEY = client.recv(SIZE)
+    publickey = Fernet(KEY)
     ok = "ACK@"
     client.send(ok.encode(FORMAT))
-
+    
     SID = client.recv(SIZE)
     client.send(ok.encode(FORMAT))
 
@@ -32,25 +30,28 @@ def main():
         # data = data[1])
 
         match cmd:
-
             case "UPLOAD":
-
                 file_path: str = fd.askopenfilename(title="Select a file")
                 print(file_path)
                 with open(file_path, 'rb') as file:
                     print(f"Sending {file_path}...")
                 file_size = os.path.getsize(file_path)
                 directorypath,filename = os.path.split(file_path)
-
                 client.send((cmd + "@" + directorypath + " | " + filename + str(file_size)).encode(FORMAT))
-
+            case "LOGIN":
+                username = input("Username: ")
+                password = input("password: ")
+                password = password.encode(FORMAT) + SID
+                print(password)
+                password = publickey.encrypt(password)
+                password = password.decode(FORMAT)
+                cmd = f"LOGIN@{username} | {password}"
+                client.sendall(cmd.encode(FORMAT))
             case "DIR":
                 client.send((cmd + '@').encode(FORMAT))
-
             case "DELETE":
                 filepath = input("File path: ")
                 client.send((cmd + "@" + filepath).encode(FORMAT))
-
             case "SUBFOLDER":
                 folderOption = input("create/delete?: ")
                 if folderOption.upper() == "CREATE":
@@ -60,10 +61,12 @@ def main():
                 if folderOption.upper() == "DELETE":
                     filePath = input("Path to Delete: ")
                     client.send((cmd + "@" + folderOption.upper() + " | " + filePath).encode(FORMAT))
-            case _:
-                client.send(cmd.encode(FORMAT))
-
+                else:
+                    print("Invalid Input")
         print(client.recv(SIZE).decode(FORMAT))
+
+
 if __name__ == "__main__":
     main()
+
     
