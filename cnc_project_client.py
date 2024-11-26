a036f9209751b6feeaea9c6813c00a77918118e1
Daniel Freer <deletes_minimal.0w@icloud.com>
2024-11-25 19:10:27 -0500

    Update cnc_project_client.py

--- cnc_project_client.py
+++ cnc_project_client.py
@@ -2,8 +2,12 @@ from enum import Enum
 import os
 import socket
 import rsa
+import pathlib
+import tkinter
+from tkinter import filedialog as fd
 
-IP = "10.180.81.31"
+
+IP = "10.180.82.224"
 PORT = int(input("Enter port number: "))
 ADDR = (IP,PORT)
 SIZE = 1024 ## byte .. buffer size
@@ -14,11 +18,11 @@ def main():
     client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     client.connect(ADDR)
     print(client.recv(SIZE).decode(FORMAT))
-      
+
     KEY = client.recv(SIZE)
     ok = "ACK@"
     client.send(ok.encode(FORMAT))
-    
+
     SID = client.recv(SIZE)
     client.send(ok.encode(FORMAT))
 
@@ -29,25 +33,37 @@ def main():
 
         match cmd:
 
-          case "DIR":
-            client.send((cmd + '@').encode(FORMAT))
+            case "UPLOAD":
 
-          case "DELETE":
-            filepath = input("File path: ")
-            client.send((cmd + "@" + filepath).encode(FORMAT))
+                file_path: str = fd.askopenfilename(title="Select a file")
+                print(file_path)
+                with open(file_path, 'rb') as file:
+                    print(f"Sending {file_path}...")
+                file_size = os.path.getsize(file_path)
+                directorypath,filename = os.path.split(file_path)
 
-          case "SUBFOLDER":
-            folderOption = input("create/delete?: ")
-            if folderOption.upper() == "CREATE":
-                filePath = input("Parent path: ")
-                dirName = input("New Directory Name: ")
-                client.send((cmd + "@" + folderOption.upper() + " | " + filePath + " | " + dirName).encode(FORMAT))
-            if folderOption.upper() == "DELETE":
-                filePath = input("Path to Delete: ")
-                client.send((cmd + "@" + folderOption.upper() + " | " + filePath).encode(FORMAT))
-          case _:
-            client.send(cmd.encode(FORMAT))
+                client.send((cmd + "@" + directorypath + " | " + filename + str(file_size)).encode(FORMAT))
+
+            case "DIR":
+                client.send((cmd + '@').encode(FORMAT))
+
+            case "DELETE":
+                filepath = input("File path: ")
+                client.send((cmd + "@" + filepath).encode(FORMAT))
+
+            case "SUBFOLDER":
+                folderOption = input("create/delete?: ")
+                if folderOption.upper() == "CREATE":
+                    filePath = input("Parent path: ")
+                    dirName = input("New Directory Name: ")
+                    client.send((cmd + "@" + folderOption.upper() + " | " + filePath + " | " + dirName).encode(FORMAT))
+                if folderOption.upper() == "DELETE":
+                    filePath = input("Path to Delete: ")
+                    client.send((cmd + "@" + folderOption.upper() + " | " + filePath).encode(FORMAT))
+            case _:
+                client.send(cmd.encode(FORMAT))
 
         print(client.recv(SIZE).decode(FORMAT))
 if __name__ == "__main__":
     main()
+    
