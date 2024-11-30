825a69c2e4fe090a0bacc198e8a0168651c4f5dd
LGNwil1 <134649981+LGNwil1@users.noreply.github.com>
2024-11-30 21:51:21 +0000

    Update cnc_project_profiler.py
    the profiler is now an importable module instead of a process

--- cnc_project_profiler.py
+++ cnc_project_profiler.py
@@ -1,25 +1,32 @@
-from enum import Enum
 import time
-import sys
+import pandas as pd
+import numpy as np
 
-#take a STRING from the server
-rcvd = sys.stdin()
+#the profiler object is meant to profile a single operation, then write its data to a file and delete
+class profiler():
+    int index
+    int start_time
+    int elapsed_time
+    int bytes
+    int data_rates[]
+    int times[]
 
-class switch(Enum):
-    time = "time"        #the server sends the stop or start time to the profiler
-    data = "data"        #the server sends a packet size
-    confirm = "confirm"  #the server tests to see if the profiler is running and connected
+    #initialize all data to 0 in a new profile
+    def __init__(self)
+        self.index = 0
+        self.start_time = 0
+        self.elapsed_time = 0
+        self.data_rates = np.zeros()
 
-#use ' | ' to delineate the command and the data, same as the client-server connection
-rcvd = rcvd.split(' | ')
-cmd = rcvd[0]
-data = rcvd[1]
+    #record the time the file operation starts
+    def start_timer():
+        start_time = time.time()
 
-if(cmd = "time"):
-    if (startTime == 0):
-        startTime = data
-    else:
-        endTime = data
-    
-if(cmd = "confirm")
-    sys.stdout.write("profiler running")
+    #compute the time the file operation took and compute the data rate
+    def stop_timer():
+        elapsed_time = start_time - time.time()
+        times[index] = elapsed_time
+        data_rates[index] = bytes/elapsed_time
+        index++
+
+    def make_csv
