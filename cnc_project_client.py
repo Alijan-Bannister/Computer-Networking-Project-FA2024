584ba397e875be97677ffc3ddadb381bb25c6c83
H4lfLif33 <166639735+H4lfLif33@users.noreply.github.com>
2024-11-30 19:09:27 -0500

    Fixed indentation issue

--- cnc_project_client.py
+++ cnc_project_client.py
@@ -131,16 +131,15 @@ def main():
           data = data.split("@")
           if data[0] == "OK":
             while True:
-              print(
-                f'{len(file_data)} / {int(data[1])}: {len(file_data) / int(data[1]) * 100:.2f}% Complete...', end='\r')
-
+              #print(f'{len(file_data)} / {int(data[1])}: {len(file_data) / int(data[1]) * 100:.2f}% Complete...', end='\r')
               file_data += client.recv(int(data[1]) - len(file_data))
               if len(file_data) >= int(data[1]):
                 break
-              local_path = os.path.join(local_path, name)
-              with open(local_path, "wb") as file:
-                file.write(file_data)
-              print("File received")
+
+            local_path = os.path.join(local_path, name)
+            with open(local_path, "wb") as file:
+              file.write(file_data)
+            print("File received")
           else:
             print("error occurred on server end")
         else:
@@ -149,4 +148,3 @@ def main():
 
 if __name__ == "__main__":
   main()
-
